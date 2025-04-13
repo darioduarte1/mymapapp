d5f78d047db47dcef4fa5b9c1ce82ecc9d85edc5
Dario Duarte <darioduarte@Mac.home>
2025-04-13 11:03:59 +0200

    chore: guardando cambios pendientes antes del rebase

--- stores/urls.py
+++ stores/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import StoreViewSet
 
 router = DefaultRouter()
 router.register(r'stores', StoreViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
