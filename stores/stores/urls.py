eee87068f0034e94e770559d3d25c081a8ca55df
Dario Duarte <darioduarte@Mac.home>
2025-04-13 16:02:50 +0200

    estrutura actualizada

--- stores/stores/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import StoreViewSet
-
-router = DefaultRouter()
-router.register(r'stores', StoreViewSet)
-
-urlpatterns = router.urls
--- stores/stores/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Store
-from .serializers import StoreSerializer
-
-class StoreViewSet(viewsets.ModelViewSet):
-    queryset = Store.objects.all()
-    serializer_class = StoreSerializer
